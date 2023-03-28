ae337e9d963d89bebe9caf0d736b32af8b74df6a
german amezquita <german.amezquita@uptc.edu.co>
2023-03-28 18:40:51 -0500

    test

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,8 @@ import co.edu.uptc.presenters.ManagerGeneral;
 
 public class Main {
     public static void main(String[] args) {
-
         ManagerGeneral.getInstance().runProject();
 
-
+// comentario
     }
 }
