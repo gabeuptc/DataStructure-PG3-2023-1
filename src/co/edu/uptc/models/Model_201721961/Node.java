de3ce56716e4c49f715e3aab23e37de34f2b9f0f
Brandon Romero <84001272+Brandon-Romero@users.noreply.github.com>
2023-03-29 17:48:23 -0500

    add button and Package model

--- src/co/edu/uptc/models/Model_201721961/Node.java
+++ src/co/edu/uptc/models/Model_201721961/Node.java
@@ -1,4 +1,39 @@
 package co.edu.uptc.models.Model_201721961;
 
 public class Node {
+
+
+
+    private int dato;
+    private Node izq, der;
+
+    public Node(int dato, Node izq, Node der) {
+        this.dato = dato;
+        this.izq = izq;
+        this.der = der;
+    }
+
+    public int getDato() {
+        return dato;
+    }
+
+    public void setDato(int dato) {
+        this.dato = dato;
+    }
+
+    public Node getIzq() {
+        return izq;
+    }
+
+    public void setIzq(Node izq) {
+        this.izq = izq;
+    }
+
+    public Node getDer() {
+        return der;
+    }
+
+    public void setDer(Node der) {
+        this.der = der;
+    }
 }
