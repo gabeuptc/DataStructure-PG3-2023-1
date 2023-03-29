117a0feadc223440631c80e911f6a76e35295559
HarDep <haroldralvaradol@gmail.com>
2023-03-29 17:56:30 -0500

    Fix Update Table
    se la actualizacion de la tabla cuando se cambia de modelo

--- src/co/edu/uptc/views/people/PanelInput.java
+++ src/co/edu/uptc/views/people/PanelInput.java
@@ -88,7 +88,7 @@ public class PanelInput extends JPanel {
         clearTable();
         DefaultTableModel tableModel = (DefaultTableModel) jTable.getModel();
         List<Person> auxList = dialogPeople.getPeople();
-        if (auxList!=null) {
+        if (!auxList.isEmpty()) {
             for (Person person : auxList) {
                 tableModel.addRow(new Object[]{person.getCode(), person.getName()});
             }
