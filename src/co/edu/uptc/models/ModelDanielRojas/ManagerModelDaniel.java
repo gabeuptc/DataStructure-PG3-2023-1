a14c74b1036e35506dbbfa0743316d65938a4dfa
RojasD13 <94007095+RojasD13@users.noreply.github.com>
2023-03-30 17:57:20 -0500

    Correción de errores

--- src/co/edu/uptc/models/ModelDanielRojas/ManagerModelDaniel.java
+++ src/co/edu/uptc/models/ModelDanielRojas/ManagerModelDaniel.java
@@ -1,7 +1,7 @@
 package co.edu.uptc.models.ModelDanielRojas;
 
 import co.edu.uptc.pojos.Person;
-import co.edu.uptc.presenters.ContratBills;
+import co.edu.uptc.presenter.ContratBills;
 
 import java.util.List;
 
