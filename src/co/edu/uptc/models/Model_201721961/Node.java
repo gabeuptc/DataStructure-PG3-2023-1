8eac40dc173ffea742f46850180a6cc9e8bb7c47
Brandon Romero <84001272+Brandon-Romero@users.noreply.github.com>
2023-03-30 15:45:44 -0500

    add class BinaryTree

--- src/co/edu/uptc/models/Model_201721961/Node.java
+++ src/co/edu/uptc/models/Model_201721961/Node.java
@@ -1,37 +1,42 @@
 package co.edu.uptc.models.Model_201721961;
+import co.edu.uptc.pojos.Person;
 
 public class Node {
 
-    private int dato;
+    private Person dato;
     private Node izq, der;
 
-    public Node(int dato, Node izq, Node der) {
+    public Node(Person dato, Node izq, Node der) {
         this.dato = dato;
         this.izq = izq;
         this.der = der;
     }
 
-    public int getDato() {
+    public Person getDato() {
         return dato;
     }
 
-    public void setDato(int dato) {
+    public void setDato(Person dato) {
         this.dato = dato;
     }
 
     public Node getIzq() {
+
         return izq;
     }
 
     public void setIzq(Node izq) {
+
         this.izq = izq;
     }
 
     public Node getDer() {
+
         return der;
     }
 
     public void setDer(Node der) {
+
         this.der = der;
     }
 }
