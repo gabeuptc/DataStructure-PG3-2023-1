29a886791ae3c955a08778176ce0b6b7a4e25db4
german.amezquita01 <german.amezquita01@uptc.edu.co>
2023-04-01 05:51:17 -0500

    generate veersion 1.0.0

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,7 @@
 import co.edu.uptc.presenter.ManagerGeneral;
-import co.edu.uptc.views.Globals.ColorPalette;
-
-import javax.swing.*;
-import javax.swing.plaf.metal.DefaultMetalTheme;
-import javax.swing.plaf.metal.MetalLookAndFeel;
-import javax.swing.plaf.metal.OceanTheme;
 
 public class Main {
-    final static String LOOKANDFEEL = "System";
-    final static String THEME = "Test";
-    public static void main(String[] args) throws UnsupportedLookAndFeelException, ClassNotFoundException, InstantiationException, IllegalAccessException {
-       // UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+    public static void main(String[] args)  {
 
         ManagerGeneral.getInstance().runProject();
     }
