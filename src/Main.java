d293587de43650f3405db91312c7bca50b004cbf
german.amezquita01 <german.amezquita01@uptc.edu.co>
2023-04-11 10:59:16 -0500

    update main

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,8 @@
 import co.edu.uptc.presenter.ManagerGeneral;
 
-import java.awt.image.BufferedImage;
-import java.net.URISyntaxException;
-
 public class Main {
-    public static void main(String[] args) throws URISyntaxException {
-
+    public static void main(String[] args) {
         ManagerGeneral.getInstance().runProject();
-
-
     }
-
-
 
 }
