5054f29be4c398955b0d7091f717d3809d113c29
q onda gente <carlos.manrique01@boomlabs.edu.co>
2023-04-19 16:28:05 -0500

    updated graph

--- src/co/edu/uptc/models/graphs/Graphs202113214/Graph.java
+++ src/co/edu/uptc/models/graphs/Graphs202113214/Graph.java
@@ -1,45 +1,157 @@
 package co.edu.uptc.models.graphs.Graphs202113214;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import co.edu.uptc.pojos.MapElement;
+import co.edu.uptc.pojos.MapRoute;
+import co.edu.uptc.views.maps.types.ElementType;
 
-import co.edu.uptc.views.maps.MapElement;
-import co.edu.uptc.views.maps.OrientationRoutes;
-import co.edu.uptc.views.maps.TypeRoute;
+import java.util.*;
+
+import static java.lang.Math.*;
 
 public class Graph {
-    private List<MapElement> nodes;
-    private List<MapElement> arcs;
-    public Graph(){
+    private Map<Integer, MapElement> elements;
+    private Map<Integer, MapElement> resultElements;
+    private int count = 0;
+    public static final int TIME = 0;
+    public static final int DISTANCE = 1;
+    public static final double RADIUS = 6371;
 
-        nodes = new ArrayList<>();
-        arcs = new ArrayList<>();
-    }
-
-
-    public void addNode(MapElement element){
-        nodes.add( element);
-    }
-    public void addArc(MapElement element){
-        arcs.add(element);
-    }
-    public  Set<MapElement> getElements(){
-        Set<MapElement> conjunto = new HashSet<>(nodes);
-        return conjunto;
-    }
-    public void setArcType(int id, TypeRoute type){
-        arcs.get(id).getMapRoute().setTypeRoute(type);
-    }
-    public void setArcSpeed(int id, float speed){   
-        arcs.get(id).getMapRoute().setSpeedRoute(speed);
-    }
-    public void setArcsOrientation(OrientationRoutes orientation){
-        //TODO implmentele miloco
+    public Graph() {
+        elements = new HashMap<>();
+        resultElements = new HashMap<>();
     }
 
-    public void deletePoint(int id){
-        nodes.remove(id);
+    public void addElement(MapElement element) {
+        element.setIdElement(count++);
+        elements.put(element.getIdElement(), element);
+    }
+
+    public Map<Integer, MapElement> getElements() {
+        return elements;
+    }
+
+    public void setElements(Map<Integer, MapElement> elements) {
+        this.elements = elements;
+    }
+
+    public void setResultElements(Map<Integer, MapElement> resultElements) {
+        this.resultElements = resultElements;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public Double getDistanceBetweenPoints(MapElement point1, MapElement point2) {
+        double lat1Rad = toRadians(point1.getGeoPosition().getLatitude());
+        double lat2Rad = toRadians(point2.getGeoPosition().getLatitude());
+
+        double deltaLat = lat2Rad - lat1Rad;
+        double deltaLon = toRadians(point2.getGeoPosition().getLongitude()) - toRadians(point1.getGeoPosition().getLongitude());
+
+        double a = sin(deltaLat / 2) * sin(deltaLat / 2) + cos(lat1Rad) * cos(lat2Rad) * sin(deltaLon / 2) * sin(deltaLon / 2);
+        double c = 2 * atan2(sqrt(a), sqrt(1 - a));
+        return RADIUS * c;
+    }
+
+    public void deleteElement(int id) {
+        elements.remove(id);
+        //TODO implementar remover
+    }
+
+    public void calculateShortestRoute(int origin, int destine, int attributeToCompare) {
+        Map<MapElement, Double> temporalValues = new HashMap<>();
+        for (MapElement point : elements.values().stream().filter(element -> element.getElementType() == ElementType.POINT).toList()) {
+            temporalValues.put(point, Double.MAX_VALUE);
+        }
+        Map<MapElement, Double> finalValues = new HashMap<>(temporalValues);
+        temporalValues.put(getElement(origin), 0.0);
+        dijkstra(temporalValues, finalValues, attributeToCompare);
+        getShortestRoute(finalValues, destine, attributeToCompare);
+    }
+
+    private void getShortestRoute(Map<MapElement, Double> finalValues, int actualPoint, int attributeToCompare) {
+        for (MapRoute child : getChildren(getElement(actualPoint))) {
+            MapElement routePoint = (getElement(actualPoint) == child.getPoint1()) ? child.getPoint2() : child.getPoint1();
+            double value = getValueOfAttribute(child, attributeToCompare);
+            if (finalValues.get(routePoint) == (finalValues.get(getElement(actualPoint)) - value)) {//Pendiente - El set deberia contener rutas y puntos?
+                resultElements.put(child.getPoint1().getIdElement(), child.getPoint1());
+                resultElements.put(child.getPoint2().getIdElement(), child.getPoint2());
+                resultElements.put(searchElementByRoute(child).getIdElement(), searchElementByRoute(child));
+                getShortestRoute(finalValues, child.getPoint1().getIdElement(), attributeToCompare);
+            } else {
+                resultElements.put(getElement(actualPoint).getIdElement(), getElement(actualPoint));
+            }
+        }
+    }
+
+    private double getValueOfAttribute(MapRoute route, int attributeToCompare) {
+        double distance = getDistanceBetweenPoints(route.getPoint1(), route.getPoint2());
+        return switch (attributeToCompare) {
+            case TIME -> distance / route.getSpeedRoute();
+            case DISTANCE -> distance;
+            default -> 0;
+        };
+    }
+
+    private void dijkstra(Map<MapElement, Double> temporalValues, Map<MapElement, Double> finalValues, int attributeToCompare) {
+        MapElement minPoint = getMinPoint(new HashMap<>(temporalValues), new HashMap<>(finalValues));
+        finalValues.put(minPoint, temporalValues.get(minPoint));
+        setTemporalValues(minPoint, getChildren(minPoint), temporalValues, attributeToCompare);
+        if (finalValues.containsValue(Double.MAX_VALUE)) {
+            dijkstra(temporalValues, finalValues, attributeToCompare);
+        }
+    }
+
+    private MapElement getMinPoint(Map<MapElement, Double> temporalValues, Map<MapElement, Double> finalValues) {
+        MapElement minKey = temporalValues.keySet().stream().min(Comparator.comparingDouble(temporalValues::get)).orElse(null);
+        if (finalValues.get(minKey) != Double.MAX_VALUE) {
+            temporalValues.remove(minKey);
+            return getMinPoint(temporalValues, finalValues);
+        }
+        return minKey;
+    }
+
+    private void setTemporalValues(MapElement minPoint, List<MapRoute> children, Map<MapElement, Double> temporalValues, int attributeToCompare) {
+        for (MapRoute child : children) {
+            MapElement point = child.getPoint1().equals(minPoint) ? child.getPoint2() : child.getPoint1();
+            double temporalValue = temporalValues.get(minPoint) + getValueOfAttribute(child, attributeToCompare);
+            if (temporalValues.get(point) == Double.MAX_VALUE) {
+                temporalValues.put(point, temporalValue);
+            } else {
+                temporalValues.put(point, Math.min(temporalValues.get(point), temporalValue));
+            }
+        }
+    }
+
+    private List<MapRoute> getChildren(MapElement actual) {
+        List<MapRoute> children = new ArrayList<>();
+        for (MapElement route : elements.values()) {
+            if (route.getElementType() == ElementType.ROUTE) {
+                if (route.getMapRoute().getPoint1().equals(actual) || route.getMapRoute().getPoint2().equals(actual)) {
+                    children.add(route.getMapRoute());
+                }
+            }
+        }
+        return children;
+    }
+
+    private MapElement searchElementByRoute(MapRoute child) {
+        for (MapElement element : elements.values()) {
+            if (element.getElementType() == ElementType.ROUTE) {
+                if (element.getMapRoute().equals(child)) {
+                    return element;
+                }
+            }
+        }
+        return null;
+    }
+
+    public MapElement getElement(int id) {
+       return elements.getOrDefault(id, null);
+    }
+
+    public Map<Integer, MapElement> getResultElements() {
+        return resultElements;
     }
 }
--- src/co/edu/uptc/models/graphs/Graphs202113214/GraphManager.java
+++ src/co/edu/uptc/models/graphs/Graphs202113214/GraphManager.java
@@ -4,16 +4,18 @@ import java.util.Set;
 
 import javax.lang.model.element.TypeElement;
 
+import co.edu.uptc.pojos.MapElement;
 import co.edu.uptc.presenter.ContractGraphs;
 import co.edu.uptc.presenter.ContractGraphs.Presenter;
 import co.edu.uptc.views.maps.ManagerGraphs;
-import co.edu.uptc.views.maps.MapElement;
 import co.edu.uptc.views.maps.MapPoint;
 import co.edu.uptc.views.maps.OrientationRoutes;
-import co.edu.uptc.views.maps.TypeRoute;
+import co.edu.uptc.views.maps.types.ElementType;
+import co.edu.uptc.views.maps.types.RouteType;
 
-public class GraphManager implements  ContractGraphs.Model {
+public class GraphManager implements   ContractGraphs.Model {
 
+    
     Graph graph = new Graph();
     public double calculateDistances(MapPoint point1, MapPoint point2){
        return  Double.parseDouble(point1.getLatitude()) - Double.parseDouble(point2.getLatitude());
@@ -25,70 +27,72 @@ public class GraphManager implements  ContractGraphs.Model {
         throw new UnsupportedOperationException("Unimplemented method 'setPresenter'");
     }
 
-    @Override
-    public Set<MapElement> calculateShortestDistanceRoute(MapPoint point1, MapPoint point2) {
-        // TODO Auto-generated method stub
-        
-    
-        throw new UnsupportedOperationException("Unimplemented method 'calculateShortestDistanceRoute'");
-    }
-
-    @Override
-    public Set<MapElement> calculateShortestTimeRoute(MapPoint point1, MapPoint point2) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'calculateShortestTimeRoute'");
-    }
-
-    @Override
-    public void setArcType(int elementID, TypeRoute typeRoute) {
-        graph.setArcType(elementID, typeRoute);
-    }
-
-    @Override
-    public void setArcSpeed(int elementID, double speed) {
-        graph.setArcSpeed(elementID, elementID);
-    }
-
-    @Override
-    public void setArcsOrientation(OrientationRoutes orientation) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setArcsOrientation'");
-    }
-
-    @Override
-    public OrientationRoutes getOrientation() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getOrientation'");
-    }
+ 
 
     @Override
     public void deletePoint(int idPoint) {
         // TODO por confirmation
-        graph.deletePoint(idPoint);
+        graph.deleteElement(idPoint);
     }
 
     @Override
     public void addElement(MapElement element) {
-       
-        co.edu.uptc.views.maps.TypeElement var = element.getTypeElement();
-        if(var.equals(co.edu.uptc.views.maps.TypeElement.ROUTE)){
-            graph.addNode(element);
-        }else if(var.equals(co.edu.uptc.views.maps.TypeElement.ROUTE)){
-
-        }
+      graph.addElement(element);
     }
 
     @Override
     public Set<MapElement> getElements() {
         // TODO confirmar q debo retornar
 
-        return graph.getElements();
+        //return graph.getElements();
+        graph.getElements();
+        return null;
+
     }
 
     @Override
     public void updateGraph() {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'updateGraph'");
+    }
+    @Override
+    public MapElement getElement(int id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getElement'");
+    }
+    @Override
+    public MapElement getElement(int idElementPoint1, int idElementPoint2) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getElement'");
+    }
+    @Override
+    public String getUser() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getUser'");
+    }
+    @Override
+    public void loadGraphs() {
+        // TODO Auto-generated method stub
+    }
+    @Override
+    public void findSortestRouteINDisntance(int idElementPoint1, int idElementPoint2) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findSortestRouteINDisntance'");
+    }
+    @Override
+    public void findShortestRouteInTime(int idElementPoint1, int idElementPoint2) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findShortestRouteInTime'");
+    }
+    @Override
+    public Set<MapElement> getResultElements() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getResultElements'");
+    }
+    @Override
+    public void modifyElement(MapElement mapElementModify) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'modifyElement'");
     }
 
 
